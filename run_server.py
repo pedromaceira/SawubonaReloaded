96ec21a2f8f881a95d244192b2b62d7e53969f81
Pedro Maceira <pedro.maceira.garcia@rai.usc.es>
2026-05-30 14:39:16 +0000

    Arreglo en el deploy para evitar el límite de tamaño de GitLab

--- run_server.py
+++ run_server.py
@@ -2,18 +2,37 @@ import sys
 import os
 import uvicorn
 import multiprocessing
+import webbrowser
+import threading
+import time
 
-# 1. TRAMPA VITAL: Le enseñamos a Python dónde buscar los archivos internos (como detector.py)
+# 1. Enseñar a Python dónde están los archivos
 ruta_actual = os.path.dirname(os.path.abspath(__file__))
 ruta_backend = os.path.join(ruta_actual, "backend")
 sys.path.insert(0, ruta_backend)
 
-# 2. Ahora ya podemos importar la app sin que las dependencias internas exploten
+# 2. Importar la API
 from backend.main import app
 
-if __name__ == "__main__":
-    # Soporte para empaquetado en Windows
-    multiprocessing.freeze_support()
+def abrir_navegador():
+    # Esperamos 3 segundos a que FastAPI arranque del todo
+    time.sleep(3)
     
+    # Construimos la ruta absoluta al archivo index.html del frontend
+    ruta_index = os.path.join(ruta_actual, "frontend", "index.html")
+    
+    # Transformamos la ruta de Windows (C:\...) a formato web (file:///C:/...)
+    url_archivo = "file:///" + ruta_index.replace("\\", "/")
+    
+    # Le decimos a Windows que lo abra
+    webbrowser.open(url_archivo)
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
     print("Iniciando SawubonaReloaded Local Server...")
+    
+    # Lanzamos el "abridor de pestañas" en segundo plano
+    threading.Thread(target=abrir_navegador, daemon=True).start()
+    
+    # Arrancamos el motor principal
     uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
